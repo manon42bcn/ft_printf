c1815bffeee8cdf425d3487e3c05510d26ea014e
Manuel Porras Ojeda <manon42bcn@yahoo.com>
2022-01-31 12:21:19 +0100

    Primer commit - estudios

---
+++ ft_printf.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mporras- <manon42bcn@yahoo.com>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/31 12:02:45 by mporras-          #+#    #+#             */
+/*   Updated: 2022/01/31 12:02:49 by mporras-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdarg.h>
+#include <stdio.h>
+
+int ft_printf(const char *str, ...)
+{
+	va_list	arguments;
+	va_start(arguments, str);
+}
+
+int main()
+{
+	
+
+	return 0;
+}
